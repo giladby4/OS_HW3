68c86b03f665bb94a718fd83399cd097a7bd9567
giladby4 <giladby4@gmail.com>
2025-01-15 10:46:52 +0000

    part 1 - problems fixed

--- server-files/server.c
+++ server-files/server.c
@@ -29,12 +29,16 @@ typedef struct {
     int real_queue_size;              // Current size of the REAL job queue
     int real_queue_front;             // Front index of the job queue
     int real_queue_rear;              // Rear index of the job queue
+    int vip_active;              // Flag indicating if VIP worker is active
+
     job_t *real_queue;            // Array of REAL jobs (requests) waiting to be processed
     pthread_mutex_t queue_lock;  // Mutex for protecting the job queue
     pthread_cond_t get_queue_not_empty;  // Condition variable to signal when the queue has jobs
     pthread_cond_t get_queue_not_full;   // Condition variable to signal when the queue is not full
     pthread_cond_t real_queue_not_empty;  // Condition variable to signal when the queue has jobs
     pthread_cond_t real_queue_not_full;   // Condition variable to signal when the queue is not full
+    pthread_cond_t vip_done;     // Signal for VIP completion
+
 
 } thread_pool;
 
@@ -46,12 +50,16 @@ void *worker_thread(void *arg) {
     while (1) {
         // Lock the queue before accessing it
         pthread_mutex_lock(&pool->queue_lock);
-
         // Wait until there is at least one job in the queue (queue is not empty)
-        while (pool->get_queue_size == 0) {
-            pthread_cond_wait(&pool->get_queue_not_empty, &pool->queue_lock);
+        while (pool->get_queue_size == 0 || pool->vip_active) {
+            if (pool->vip_active) {
+                pthread_cond_wait(&pool->vip_done, &pool->queue_lock);
+            } else {
+                pthread_cond_wait(&pool->get_queue_not_empty, &pool->queue_lock);
+            }
         }
 
+            
         // Remove the job from the queue (circular queue mechanism)
         job = pool->get_queue[pool->get_queue_front];
         pool->get_queue_front = (pool->get_queue_front + 1) % pool->max_queue_size;  // Move the front pointer
@@ -59,7 +67,7 @@ void *worker_thread(void *arg) {
 
         // Signal that the queue is not full (so more jobs can be added)
         pthread_cond_signal(&pool->get_queue_not_full);
-
+        
         // Unlock the queue after accessing it
         pthread_mutex_unlock(&pool->queue_lock);
 
@@ -84,6 +92,7 @@ void *worker_thread(void *arg) {
         // Optionally, you can close the connection if no further communication is required
         close(job.connfd);
     }
+    
 
     return NULL;
 }
@@ -100,32 +109,45 @@ void *vip_worker_thread(void *arg) {
         while (pool->real_queue_size == 0) {
             pthread_cond_wait(&pool->real_queue_not_empty, &pool->queue_lock);
         }
+        pool->vip_active = 1;
 
-        // Get the job from the VIP queue (circular queue)
-        job = pool->real_queue[pool->real_queue_front];
-        pool->real_queue_front = (pool->real_queue_front + 1) % pool->max_queue_size;
-        pool->real_queue_size--;
+        while (pool->real_queue_size > 0) {
 
-        // Signal that the VIP queue is not full
-        pthread_cond_signal(&pool->real_queue_not_full);
+            // Get the job from the VIP queue (circular queue)
+            job = pool->real_queue[pool->real_queue_front];
+            pool->real_queue_front = (pool->real_queue_front + 1) % pool->max_queue_size;
+            pool->real_queue_size--;
+
+            // Signal that the VIP queue is not full
+            pthread_cond_signal(&pool->real_queue_not_full);
+
+            pthread_mutex_unlock(&pool->queue_lock);
+
+            // Process the VIP request
+            struct timeval arrival, dispatch;
+            arrival.tv_sec = 0; arrival.tv_usec = 0;
+            dispatch.tv_sec = 0; dispatch.tv_usec = 0;
+
+            // Example: VIP request handling (change according to actual request handling)
+            threads_stats t_stats = malloc(sizeof(threads_stats));
+            t_stats->id = 0;
+            t_stats->stat_req = 0;
+            t_stats->dynm_req = 0;
+            t_stats->total_req = 0;
+            requestHandle(job.connfd, arrival, dispatch, t_stats);
+            free(t_stats);
+
+            close(job.connfd);
+
+            pthread_mutex_lock(&pool->queue_lock);
+
+        }
+        pool->vip_active = 0;
+        pthread_cond_broadcast(&pool->vip_done);
 
         pthread_mutex_unlock(&pool->queue_lock);
 
-        // Process the VIP request
-        struct timeval arrival, dispatch;
-        arrival.tv_sec = 0; arrival.tv_usec = 0;
-        dispatch.tv_sec = 0; dispatch.tv_usec = 0;
 
-        // Example: VIP request handling (change according to actual request handling)
-        threads_stats t_stats = malloc(sizeof(threads_stats));
-        t_stats->id = 0;
-        t_stats->stat_req = 0;
-        t_stats->dynm_req = 0;
-        t_stats->total_req = 0;
-        requestHandle(job.connfd, arrival, dispatch, t_stats);
-        free(t_stats);
-
-        close(job.connfd);
     }
 
     return NULL;
@@ -206,6 +228,8 @@ int main(int argc, char *argv[])
     pool->real_queue_size = 0; 
     pool->real_queue_front = 0;
     pool->real_queue_rear = 0;
+    pool->vip_active = 0;
+
 
     pthread_mutex_init(&pool->queue_lock, NULL);
     pthread_cond_init(&pool->get_queue_not_empty, NULL);
